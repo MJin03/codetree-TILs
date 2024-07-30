be16d6f7dea42549012470f9b33354d67074bd3a
MJin03 <147484048+MJin03@users.noreply.github.com>
2024-07-31 00:56:22 +0900

    Title: 홀수만 출력, Time: 186ms, Memory: 47MB, Status: Passed - CodeTree

---
+++ 240731/홀수만 출력/output-only-odd.js
@@ -0,0 +1,15 @@
+const fs = require("fs");
+let n = fs.readFileSync(0).toString().trim().split(" ");
+
+let a = Number(n[0]);
+let b = Number(n[1]);
+
+let result = "";
+
+for (let i=a; i<=b; i++) {
+    if (i%2==1) {
+        result += (i + " ");
+    }
+}
+
+console.log(result);
