6d3d13d28e87427d7ec87f5fd9608448101b895a
MJin03 <147484048+MJin03@users.noreply.github.com>
2024-07-30 22:14:06 +0900

    Title: 알파벳에 따른 평가, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240730/알파벳에 따른 평가/evaluation-by-alphabet.js
@@ -0,0 +1,21 @@
+const fs = require("fs");
+let n = fs.readFileSync(0).toString().trim();
+
+if (n == 'S') {
+    console.log("Superior")
+}
+else if (n == 'A') {
+    cnosole.log("Excellent")
+}
+else if (n == 'B') {
+    console.log("Good")
+}
+else if (n == 'C') {
+    console.log("Usually")
+}
+else if (n == 'D') {
+    console.log("Effort")
+}
+else {
+    console.log("Failure")
+}
