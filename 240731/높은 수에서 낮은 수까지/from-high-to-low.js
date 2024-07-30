2a50c307094977b3d4c73ea31a04a77834f8edc7
MJin03 <147484048+MJin03@users.noreply.github.com>
2024-07-31 01:45:54 +0900

    Title: 높은 수에서 낮은 수까지, Time: 196ms, Memory: 14MB, Status: Passed - CodeTree

---
+++ 240731/높은 수에서 낮은 수까지/from-high-to-low.js
@@ -0,0 +1,20 @@
+const fs = require("fs");
+let n = fs.readFileSync(0).toString().trim().split(" ");
+
+let a = Number(n[0]);
+let b = Number(n[1]);
+
+let result = "";
+
+if (a>b) {
+    for (let i=a; i>=b; i--) {
+        result += i + " ";
+    }
+}
+else {
+    for (let i=b; i>=a; i--) {
+        result += i + " ";
+    }
+}
+
+console.log(result);
