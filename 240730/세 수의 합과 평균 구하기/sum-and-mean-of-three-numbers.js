436ab6499c8c5433fcc97d7e97f2b4f2f27a7788
MJin03 <147484048+MJin03@users.noreply.github.com>
2024-07-30 21:16:38 +0900

    Title: 세 수의 합과 평균 구하기, Time: 162ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 240730/세 수의 합과 평균 구하기/sum-and-mean-of-three-numbers.js
@@ -0,0 +1,10 @@
+const fs = require("fs");
+let n = fs.readFileSync(0).toString().trim().split(" ");
+
+let a = Number(n[0]);
+let b = Number(n[1]);
+let c = Number(n[2]);
+
+let plus = a + b + c;
+
+console.log(`${plus}\n${(plus/3).toFixed(0)}`)
