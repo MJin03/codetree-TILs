f29509298d91a4dc868712434ae3b045d9bc46cb
MJin03 <147484048+MJin03@users.noreply.github.com>
2024-07-31 02:13:01 +0900

    Title: 369 게임, Time: 179ms, Memory: 14MB, Status: Wrong Answer - CodeTree

--- 240731/369 게임/369-game.js
+++ 240731/369 게임/369-game.js
@@ -5,10 +5,10 @@ let result = "";
 
 for (let i=1; i<=n; i++) {
     if (i%3==0) {
-        result += 0 + " ";
+        result += "0 ";
     }
     else if ((i/10==3) || (i/10==6) || (i/10==9)) {
-        result += 0 + " ";
+        result += "0 ";
     }
     else {
         result += i + " ";
