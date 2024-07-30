a7e2cedcd29ad345e93556654a696ad69e136249
MJin03 <147484048+MJin03@users.noreply.github.com>
2024-07-31 01:48:37 +0900

    Title: 자연수면 출력, Time: 174ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 240731/자연수면 출력/output-only-natural-number.js
@@ -0,0 +1,15 @@
+const fs = require("fs");
+let n = fs.readFileSync(0).toString().trim().split(" ");
+
+let a = Number(n[0]);
+let b = Number(n[1]);
+
+let result = "";
+
+if (a>0) {
+    for (let i=1; i<=b; i++) {
+        result += a + "";
+    }
+}
+
+console.log(result);
