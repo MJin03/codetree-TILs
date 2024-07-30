50abd85aad44d39e3053c8ac2295b6912b192ade
MJin03 <147484048+MJin03@users.noreply.github.com>
2024-07-30 19:40:17 +0900

    Title: 간단한 사칙연산, Time: 205ms, Memory: 15MB, Status: Passed - CodeTree

---
+++ 240730/간단한 사칙연산/simple-arithmetic-operation.js
@@ -0,0 +1,10 @@
+const fs = require("fs");
+let n = fs.readFileSync(0).toString().trim().split(" ");
+
+let a = Number(n[0]);
+let b = Number(n[1]);
+
+console.log(a+b);
+console.log(a-b);
+console.log(parseInt(a/b));
+console.log(a%b);
