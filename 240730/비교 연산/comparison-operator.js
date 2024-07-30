1e6f5a2f39f6334189b8f9811d2001b20bcc13f0
MJin03 <147484048+MJin03@users.noreply.github.com>
2024-07-30 22:49:30 +0900

    Title: 비교 연산, Time: 185ms, Memory: 14MB, Status: Passed - CodeTree

---
+++ 240730/비교 연산/comparison-operator.js
@@ -0,0 +1,12 @@
+const fs = require("fs");
+let n = fs.readFileSync(0).toString().trim().split(" ");
+
+let a = Number(n[0]);
+let b = Number(n[1]);
+
+console.log(a >= b ? 1 : 0)
+console.log(a > b ? 1 : 0)
+console.log(b >= a ? 1 : 0)
+console.log(b > a ? 1 : 0)
+console.log(a==b ? 1 : 0)
+console.log(a != b ? 1 : 0)
